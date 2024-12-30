56b40771ea8488e552de39949dd924dbd3329433
abhimessi16 <piabhi18@gmail.com>
2024-12-30 19:09:45 +0530

    chore: added default language for translation

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from app.routes.stt_route import stt_router
 
 processor = WhisperProcessor.from_pretrained("openai/whisper-small", language="en", task="transcribe")
 model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
-model.config.forced_decoder_ids = None
 
 app = FastAPI()
 app.include_router(stt_router)
