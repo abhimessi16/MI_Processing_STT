bedee94349164c980d0304e0169cad3c8f06e63c
abhimessi16 <piabhi18@gmail.com>
2025-01-01 21:21:07 +0530

    chore: app name refactor

--- app/main.py
+++ app/main.py
@@ -6,5 +6,5 @@ from app.routes.stt_route import stt_router
 processor = WhisperProcessor.from_pretrained("openai/whisper-small", language="en", task="transcribe")
 model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
 
-app = FastAPI()
-app.include_router(stt_router)
+stt_app = FastAPI()
+stt_app.include_router(stt_router)
